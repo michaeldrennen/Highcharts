d78b248d7f622b4efd415bdebd6aa5c7cc6160bf
Michael Drennen <michaeldrennen74@gmail.com>
2018-09-09 12:02:46 -0600

    Forgot to add the autoloader into composer.json.

--- src/Highchart.php
+++ src/Highchart.php
@@ -43,9 +43,8 @@ class Highchart {
 
 
     public function script(): string {
-        $script = "var Highcharts = require('highcharts');";
-        $script .= "require('highcharts/modules/exporting')(Highcharts);";
-        $script .= "var highchart_" . $this->id . " = Highchart.chart('highchart_container_" . $this->id . "', " . json_encode( $this->options ) . ");";
+        $script = '<script src="https://code.highcharts.com/highcharts.js"></script>';
+        $script .= "<script>var highchart_" . $this->id . " = Highchart.chart('highchart_container_" . $this->id . "', " . json_encode( $this->options ) . ");</script>";
         return $script;
     }
 
