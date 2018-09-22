92589cec8127f0cc7e0c1fbe2f6658e76188dbe8
Michael Drennen <michaeldrennen74@gmail.com>
2018-09-22 17:05:06 -0600

    Added the ability to use highcharts or highstock.

--- src/Highchart.php
+++ src/Highchart.php
@@ -2,8 +2,18 @@
 
 namespace MichaelDrennen\Highcharts;
 
-use phpDocumentor\Reflection\Types\Self_;
 
+/**
+ * Class Highchart
+ * @package MichaelDrennen\Highcharts
+ *
+ *          Most recent files for Highcharts
+http://code.highcharts.com/highcharts.js
+http://code.highcharts.com/highcharts-more.js
+http://code.highcharts.com/modules/exporting.js
+http://code.highcharts.com/adapters/mootools-adapter.js
+http://code.highcharts.com/adapters/prototype-adapter.js
+ */
 class Highchart {
 
     protected $id = '';
@@ -12,12 +22,26 @@ class Highchart {
     //protected $title    = '';
     //protected $subTitle = '';
 
+    protected $type   = 'highstock'; // highstock, highchart
     protected $height = 400;
     protected $width  = '100%';
+    protected $script = '';
 
+    /**
+     * @var bool I only need to include the highcharts js files once.
+     */
     public static $scriptLoaded = FALSE;
 
-    protected function __construct() {
+    /**
+     * Highchart constructor.
+     * @param string $type
+     * @param int    $height
+     * @param string $width
+     */
+    protected function __construct( string $type = 'highstock', $height = 400, $width = '100%' ) {
+        $this->type   = $type;
+        $this->height = $height;
+        $this->width  = $width;
         $this->setIdOfChart();
     }
 
@@ -28,38 +52,47 @@ class Highchart {
         return new Highchart();
     }
 
+    /**
+     * @param array $options
+     * @return $this
+     */
     public function options( array $options ) {
         $this->options = $options;
         return $this;
     }
 
-//    public function title( string $title ) {
-//        $this->title = $title;
-//        return $this;
-//    }
-//
-//    public function subTitle( string $subTitle ) {
-//        $this->subTitle = $subTitle;
-//        return $this;
-//    }
+    /**
+     * Only load the scripts that you need.
+     * TODO Add logic to selectively load the more/panes/exporting scripts if they're not needed.
+     * @throws \Exception
+     */
+    protected function setScripts() {
+        $this->script = '';
+        switch ( $this->type ):
+            case 'highstock':
+                $this->script .= "\n" . '<script src="https://code.highcharts.com/stock/highchart.js"></script> ';
+                break;
+            case 'highchart':
+                $this->script .= "\n" . '<script src="https://code.highcharts.com/stock/highstock.js"></script> ';
+                break;
+            default:
+                throw new \Exception( "You tried to set the type of this chart to something that isn't supported." );
+        endswitch;
+
+        $this->script .= "\n" . '<script src="https://code.highcharts.com/stock/highcharts-more.js"></script> ';
+        $this->script .= "\n" . '<script src="https://code.highcharts.com/stock/modules/drag-panes.js"></script> ';
+        $this->script .= "\n" . '<script src="https://code.highcharts.com/stock/modules/exporting.js"></script> ';
+    }
 
 
     public function script(): string {
-        $script = '';
-
         // Only load these files onto the page once.
         if ( FALSE === self::$scriptLoaded ):
-            //$script = '<script src="https://code.highcharts.com/highcharts.js"></script>';
-            $script             .= "\n" . '<script src="https://code.highcharts.com/stock/highstock.js"></script> ';
-            $script             .= "\n" . '<script src="https://code.highcharts.com/stock/highcharts-more.js"></script> ';
-            $script             .= "\n" . '<script src="https://code.highcharts.com/stock/modules/drag-panes.js"></script> ';
-            $script             .= "\n" . '<script src="https://code.highcharts.com/stock/modules/exporting.js"></script> ';
+            $this->setScripts();
             self::$scriptLoaded = TRUE;
         endif;
-
-        $script .= "<script>var highchart_" . $this->id . " = Highcharts.stockChart('highchart_container_" . $this->id . "', " . json_encode( $this->options,
-                                                                                                                                              JSON_UNESCAPED_SLASHES ) . ");</script>";
-        return $script;
+        $this->script .= "<script>var highchart_" . $this->id . " = Highcharts.stockChart('highchart_container_" . $this->id . "', " . json_encode( $this->options,                                                                                                                                      JSON_UNESCAPED_SLASHES ) . ");</script>";
+        return $this->script;
     }
 
     protected function setIdOfChart() {
